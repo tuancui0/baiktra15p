bce7124e95c16e4d39d83d94385cd65d00484e45
tuancui0 <142402967+tuancui0@users.noreply.github.com>
2024-11-06 02:28:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/baiktra15p/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.baiktra15p;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+Button calculate;
+Button reset;
+TextView result;
+CheckBox checkBox;
+EditText height;
+EditText weight;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        calculate = findViewById(R.id.btn_calculate);
+        reset = findViewById(R.id.btn_reset);
+        checkBox = findViewById(R.id.checkBox);
+        result = findViewById(R.id.txt_result);
+        height = findViewById(R.id.txt_height);
+        weight = findViewById(R.id.txt_weight);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    double chieucao = Double.parseDouble(height.getText().toString());
+                    double cannang = Double.parseDouble(weight.getText().toString());
+                    double ketqua = cannang / (chieucao * chieucao);
+
+                    result.setText(String.format("BMI: %.2f", ketqua));
+                } catch (NumberFormatException e) {
+                    result.setText("Vui long nhap dung can nang va chieu cao");
+                }
+            }
+        });
+    }
+}
